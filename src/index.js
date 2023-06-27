99ab84d1e016ff49f0c6b57467cee704f7782901
Muccachino <lucas_staszewski@yahoo.de>
2023-06-27 13:45:54 +0200

    weatherdata can be fetched and shown in console

--- src/index.js
+++ src/index.js
@@ -1,2 +1,21 @@
 import "./styles.scss";
-console.log("hello world!");
+
+async function fetchWeatherData() {
+  try {
+    const response = await fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=graz&appid=dcdb46266f021e65cfdf86ea280dcece"
+    );
+    const data = await response.json();
+    console.log(data);
+    //showData(data);
+  } catch (err) {
+    throw new Error("Sorry, something went wrong");
+  }
+}
+fetchWeatherData();
+
+/* const showData = (data) => {
+  const container = document.querySelector("#app");
+  const weather = document.createElement("div")
+
+} */
