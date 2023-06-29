32ff5482a63c3a1a2dc4e1e87ce094c62d745193
Muccachino <lucas_staszewski@yahoo.de>
2023-06-29 13:50:21 +0200

    input for city name added, weather data now shown correctly, icon displayed depending on weather condition

--- src/index.js
+++ src/index.js
@@ -1,21 +1,63 @@
 import "./styles.scss";
+import {
+  createTags,
+  expandHtml,
+  addPicture,
+  createSwitch,
+} from "./modules/functions";
 
-async function fetchWeatherData() {
+async function fetchWeatherData(city) {
   try {
     const response = await fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=graz&appid=dcdb46266f021e65cfdf86ea280dcece"
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=dcdb46266f021e65cfdf86ea280dcece`
     );
-    const data = await response.json();
-    console.log(data);
-    //showData(data);
+    const alldata = await response.json();
+    const mydata = [
+      [alldata.name, alldata.sys.country],
+      `${(alldata.main.temp - 273.15).toFixed(1)} °C`,
+      `Min: ${(alldata.main.temp_min - 273.15).toFixed(1)} °C`,
+      `Max: ${(alldata.main.temp_max - 273.15).toFixed(1)} °C`,
+      alldata.weather[0].description,
+      alldata.weather[0].icon,
+    ];
+    showData(mydata);
   } catch (err) {
     throw new Error("Sorry, something went wrong");
   }
 }
-fetchWeatherData();
 
-/* const showData = (data) => {
-  const container = document.querySelector("#app");
-  const weather = document.createElement("div")
+const showData = (data) => {
+  container.innerHTML = "";
+  for (let i = 0; i < data.length - 1; i++) {
+    const weather = document.createElement("p");
+    weather.innerHTML = data[i];
+    weather.classList.add(`data${i}`);
+    container.appendChild(weather);
+  }
+  const icon = addPicture(
+    `https://openweathermap.org/img/wn/${data[5]}@2x.png`,
+    container
+  );
+  const tempSwitch = createSwitch(container);
+  const tempInfo = createTags("span", "cel-fa", null, "°C / °F");
+  expandHtml(container, tempInfo);
+};
 
-} */
+const cityInput = createTags("input", null, "cityInput");
+cityInput.type = "text";
+cityInput.placeholder = "City";
+expandHtml(document.body, cityInput);
+
+const container = createTags("div", null, "container");
+expandHtml(document.body, container);
+
+cityInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    const city = cityInput.value;
+    fetchWeatherData(city);
+    container.style.backgroundColor = "skyblue";
+  }
+});
+
+const tempBox = document.querySelector(".switchbox");
+//tempBox.addEventListener;
---
+++ src/modules/functions.js
@@ -0,0 +1,37 @@
+"use strict";
+
+function createTags(htmlTags, className, idName, innerHtml) {
+  let element = document.createElement(htmlTags);
+  if (className != null) {
+    element.classList.add(className);
+  }
+  if (idName != null) {
+    element.id = idName;
+  }
+  if (innerHtml != null) {
+    element.innerHTML = innerHtml;
+  }
+  return element;
+}
+
+function expandHtml(expand, tags) {
+  expand.appendChild(tags);
+}
+
+function addPicture(picImportName, parent) {
+  let myPic = new Image();
+  myPic.src = picImportName;
+  parent.appendChild(myPic);
+}
+
+function createSwitch(parent) {
+  const label = createTags("label", "switch");
+  const box = createTags("input", "switchbox");
+  box.type = "checkbox";
+  const slider = createTags("span", "slider");
+  expandHtml(label, box);
+  expandHtml(label, slider);
+  expandHtml(parent, label);
+}
+
+export { createTags, expandHtml, addPicture, createSwitch };
